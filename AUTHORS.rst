d0b11a2252da95a19ee41587283416a3091a0796
Ilya Otyutskiy <ilya.otyutskiy@icloud.com>
2014-03-10 21:59:52 +0400

    New release: 1.0.

---
+++ AUTHORS.rst
@@ -0,0 +1,8 @@
+Leaf contributors
+=================
+
+Leaf is written by `Roman Koblov <https://github.com/penpen>`_ with contributions from:
+
+- `imbolc <https://github.com/imbolc>`_ (Fixes)
+- `Fedor Gogolev <https://github.com/knsd>`_ (Fixes)
+- `Ilya Otyutskiy <https://github.com/thesharp>`_ (Porting to Python3, release engineering)
--- setup.py
+++ setup.py
@@ -3,11 +3,13 @@
 from setuptools import setup, find_packages
 import os.path
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-setup(name='leaf',
-    version='0.4.5',
+setup(
+    name='leaf',
+    version='1.0',
     description='Simple Python library for HTML parsing',
     author='Roman Koblov',
     author_email='pingu.g@gmail.com',
@@ -16,18 +18,21 @@ setup(name='leaf',
     keywords=['html', 'parsing', 'web scrapping'],
     packages=['leaf'],
     data_files=[
-            ('', ['LICENSE', 'README.rst'])
+            ('', ['LICENSE', 'README.rst', 'AUTHORS.rst'])
     ],
     package_data={
         '': ['*.txt', '*.rst', '*.md']
     },
-    classifiers=[
-         'Development Status :: 3 - Alpha',
-         'Intended Audience :: Developers',
-         'License :: OSI Approved :: MIT License',
-         'Operating System :: OS Independent',
-         'Programming Language :: Python',
-         'Topic :: Scientific/Engineering :: Information Analysis'],
-    install_requires = ['setuptools', 'lxml'],
+    classifiers=['Development Status :: 5 - Production/Stable',
+                 'Intended Audience :: Developers',
+                 'License :: OSI Approved :: MIT License',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python',
+                 'Programming Language :: Python :: 2.6',
+                 'Programming Language :: Python :: 2.7',
+                 'Programming Language :: Python :: 3',
+                 'Programming Language :: Python :: 3.3',
+                 'Topic :: Scientific/Engineering :: Information Analysis'],
+    install_requires=['setuptools', 'lxml'],
     long_description=read('README.rst')+read('CHANGES.rst'),
 )
